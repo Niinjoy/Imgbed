08fa5ddaaaa697caf182c8796a8f54839172b027
Niinjoy <39721307+Niinjoy@users.noreply.github.com>
2021-06-23 21:07:28 +0800

    add pll image

--- getImg.py
+++ getImg.py
@@ -1,9 +1,8 @@
-# from skimage import io
-# img_url = "http://cube.rider.biz/visualcube.php?fmt=svg&case=URU'R'&stage=f2l&bg=t&view=trans&cc=grey&co=40&size=512"
-# image = io.imread(img_url)
-# # io.imshow(image)
-# # io.show()
-# io.imsave('test.svg',image)
+import urllib.request, csv
 
-import urllib.request
-urllib.request.urlretrieve("http://cube.rider.biz/visualcube.php?fmt=svg&case=URU'R'&stage=f2l&bg=t&view=trans&cc=grey&co=40", "cubeImg/test2.svg")
+ganpll = csv.reader(open('GANPLL.csv', newline=''))
+for row in ganpll:
+	urllib.request.urlretrieve("http://cube.rider.biz/visualcube.php?fmt=svg&view=plan&case="+row[1], "cubeImg/"+row[0]+".svg")
+
+# urllib.request.urlretrieve("http://cube.rider.biz/visualcube.php?fmt=svg&case=URU'R'&stage=f2l&bg=t&view=trans&cc=grey&co=40", "cubeImg/test2.svg")
+# urllib.request.urlretrieve("http://cube.rider.biz/visualcube.php?fmt=svg&view=plan&case=", "cubeImg/test2.svg")
