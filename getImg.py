f79471d4eb741c52572fec8ed51e4da3d3e0677b
Niinjoy <39721307+Niinjoy@users.noreply.github.com>
2021-06-23 21:45:38 +0800

    trans bg

--- getImg.py
+++ getImg.py
@@ -2,7 +2,7 @@ import urllib.request, csv
 
 ganpll = csv.reader(open('GANPLL.csv', newline=''))
 for row in ganpll:
-	urllib.request.urlretrieve("http://cube.rider.biz/visualcube.php?fmt=svg&view=plan&case="+row[1], "cubeImg/"+row[0]+".svg")
+	urllib.request.urlretrieve("http://cube.rider.biz/visualcube.php?fmt=svg&view=plan&bg=t&case="+row[1], "cubeImg/"+row[0]+".svg")
 
 # urllib.request.urlretrieve("http://cube.rider.biz/visualcube.php?fmt=svg&case=URU'R'&stage=f2l&bg=t&view=trans&cc=grey&co=40", "cubeImg/test2.svg")
 # urllib.request.urlretrieve("http://cube.rider.biz/visualcube.php?fmt=svg&view=plan&case=", "cubeImg/test2.svg")
